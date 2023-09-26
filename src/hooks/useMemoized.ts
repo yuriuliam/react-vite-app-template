a46a9da1000dccea8b79b69b5b6adb0b81903118
Yuri Uliam <21960752+yuriuliam@users.noreply.github.com>
2023-09-25 21:05:23 -0300

    fix(hooks): updated useMemoized to accept external ttl parameter

--- src/hooks/useMemoized.ts
+++ src/hooks/useMemoized.ts
@@ -5,7 +5,10 @@ import { memoize } from '@/utils/functions'
 /**
  * Memoizes the return value of a given callback based on their parameters.
  */
-const useMemoized = <T extends (...args: any[]) => any>(callback: T | null) => {
+const useMemoized = <T extends (...args: any[]) => any>(
+  callback: T | null,
+  ttl = 0,
+) => {
   const callbackRef = React.useRef<T | null>(null)
   const cacheRef = React.useRef(new Map())
 
@@ -17,8 +20,9 @@ const useMemoized = <T extends (...args: any[]) => any>(callback: T | null) => {
     () =>
       memoize((...args: any[]) => callbackRef.current?.(...args) as T, {
         cache: cacheRef.current,
+        ttl,
       }),
-    [],
+    [ttl],
   )
 }
 
