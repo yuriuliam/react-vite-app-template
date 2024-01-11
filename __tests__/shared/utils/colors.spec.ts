526e2cc0f8f1e05e2764d73502ec49a005abc621
Yuri Uliam <21960752+yuriuliam@users.noreply.github.com>
2024-01-11 10:45:16 -0300

    tests(colors): added tests for hsla, hsv, rgba and rgba32

--- __tests__/shared/utils/colors.spec.ts
+++ __tests__/shared/utils/colors.spec.ts
@@ -4,67 +4,68 @@ import {
   argb32,
   cmyk,
   hex,
-  // hsla,
-  // hsv,
-  // rgba,
-  // rgba32,
+  hsla,
+  hsv,
+  rgba,
+  rgba32,
 } from '@/shared/utils/colors'
 
-const COLOR_VALUES = Object.freeze({
+/** Color values - Based on HSLA conversions */
+const COLOR_VALUES_HSLA_BASED = Object.freeze({
   red: Object.freeze({
-    argb32: 0xff_cc_36_10 & 0xff_ff_ff_ff,
-    cmyk: Object.freeze({ cyan: 0, magenta: 74, yellow: 92, key: 20 }),
-    hex: '#cc3610ff',
+    argb32: 0xff_cb_36_10 & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 0, magenta: 73, yellow: 92, key: 20 }),
+    hex: '#cb3610ff',
     hsla: Object.freeze({ hue: 12, saturation: 85, luminance: 43, alpha: 100 }),
     hsv: Object.freeze({ hue: 12, saturation: 92, vibrance: 80 }),
-    rgba: Object.freeze({ red: 204, green: 54, blue: 16, alpha: 255 }),
-    rgba32: 0xcc_36_10_ff,
+    rgba: Object.freeze({ red: 203, green: 54, blue: 16, alpha: 255 }),
+    rgba32: 0xcb_36_10_ff,
   }),
   orange: Object.freeze({
-    argb32: 0xff_d9_90_1a & 0xff_ff_ff_ff,
-    cmyk: Object.freeze({ cyan: 0, magenta: 34, yellow: 88, key: 15 }),
-    hex: '#d9901aff',
+    argb32: 0xff_db_91_1a & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 0, magenta: 34, yellow: 88, key: 14 }),
+    hex: '#db911aff',
     hsla: Object.freeze({ hue: 37, saturation: 79, luminance: 48, alpha: 100 }),
-    hsv: Object.freeze({ hue: 37, saturation: 88, vibrance: 85 }),
-    rgba: Object.freeze({ red: 217, green: 144, blue: 26, alpha: 255 }),
-    rgba32: 0xd9_90_1a_ff,
+    hsv: Object.freeze({ hue: 37, saturation: 88, vibrance: 86 }),
+    rgba: Object.freeze({ red: 219, green: 145, blue: 26, alpha: 255 }),
+    rgba32: 0xdb_91_1a_ff,
   }),
   yellow: Object.freeze({
-    argb32: 0xff_d6_bd_18 & 0xff_ff_ff_ff,
-    cmyk: Object.freeze({ cyan: 0, magenta: 12, yellow: 89, key: 16 }),
-    hex: '#d6bd18ff',
+    argb32: 0xff_d8_be_18 & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 0, magenta: 12, yellow: 89, key: 15 }),
+    hex: '#d8be18ff',
     hsla: Object.freeze({ hue: 52, saturation: 80, luminance: 47, alpha: 100 }),
-    hsv: Object.freeze({ hue: 52, saturation: 89, vibrance: 84 }),
-    rgba: Object.freeze({ red: 214, green: 189, blue: 24, alpha: 255 }),
-    rgba32: 0xd6_bd_18_ff,
+    hsv: Object.freeze({ hue: 52, saturation: 89, vibrance: 85 }),
+    rgba: Object.freeze({ red: 216, green: 190, blue: 24, alpha: 255 }),
+    rgba32: 0xd8_be_18_ff,
   }),
   green: Object.freeze({
-    argb32: 0xff_5d_d6_3c & 0xff_ff_ff_ff,
-    cmyk: Object.freeze({ cyan: 57, magenta: 0, yellow: 72, key: 16 }),
-    hex: '#5dd63cff',
+    argb32: 0xff_5e_d6_3d & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 56, magenta: 0, yellow: 71, key: 16 }),
+    hex: '#5ed63dff',
     hsla: Object.freeze({
       hue: 107,
       saturation: 65,
       luminance: 54,
       alpha: 100,
     }),
-    hsv: Object.freeze({ hue: 107, saturation: 72, vibrance: 84 }),
-    rgba: Object.freeze({ red: 93, green: 214, blue: 60, alpha: 255 }),
-    rgba32: 0x5d_d6_3c_ff,
+    hsv: Object.freeze({ hue: 107, saturation: 71, vibrance: 84 }),
+    rgba: Object.freeze({ red: 94, green: 214, blue: 61, alpha: 255 }),
+    rgba32: 0x5e_d6_3d_ff,
   }),
   blue: Object.freeze({
-    argb32: 0xff_21_74_b8 & 0xff_ff_ff_ff,
-    cmyk: Object.freeze({ cyan: 82, magenta: 37, yellow: 0, key: 28 }),
-    hex: '#2174b8ff',
+    argb32: 0xff_21_75_ba & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 82, magenta: 37, yellow: 0, key: 27 }),
+    hex: '#2175baff',
     hsla: Object.freeze({
       hue: 207,
       saturation: 70,
       luminance: 43,
       alpha: 100,
     }),
-    hsv: Object.freeze({ hue: 207, saturation: 82, vibrance: 72 }),
-    rgba: Object.freeze({ red: 33, green: 116, blue: 184, alpha: 255 }),
-    rgba32: 0x21_74_b8_ff,
+    hsv: Object.freeze({ hue: 207, saturation: 82, vibrance: 73 }),
+    rgba: Object.freeze({ red: 33, green: 117, blue: 186, alpha: 255 }),
+    rgba32: 0x21_75_ba_ff,
   }),
   purple: Object.freeze({
     argb32: 0xff_74_32_b3 & 0xff_ff_ff_ff,
@@ -81,24 +82,111 @@ const COLOR_VALUES = Object.freeze({
     rgba32: 0x74_32_b3_ff,
   }),
   magenta: Object.freeze({
-    argb32: 0xff_db_1d_c8 & 0xff_ff_ff_ff,
-    cmyk: Object.freeze({ cyan: 0, magenta: 87, yellow: 9, key: 14 }),
-    hex: '#db1dc8ff',
+    argb32: 0xff_dd_1d_ca & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 0, magenta: 87, yellow: 9, key: 13 }),
+    hex: '#dd1dcaff',
     hsla: Object.freeze({
       hue: 306,
       saturation: 77,
       luminance: 49,
       alpha: 100,
     }),
-    hsv: Object.freeze({ hue: 306, saturation: 87, vibrance: 86 }),
-    rgba: Object.freeze({ red: 219, green: 29, blue: 200, alpha: 255 }),
-    rgba32: 0xdb_1d_c8_ff,
+    hsv: Object.freeze({ hue: 306, saturation: 87, vibrance: 87 }),
+    rgba: Object.freeze({ red: 221, green: 29, blue: 202, alpha: 255 }),
+    rgba32: 0xdd_1d_ca_ff,
+  }),
+})
+
+/** Color values - Based on HSV conversions */
+const COLOR_VALUES_HSV_BASED = Object.freeze({
+  red: Object.freeze({
+    argb32: 0xff_cc_36_10 & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 0, magenta: 74, yellow: 92, key: 20 }),
+    hex: '#cc3610ff',
+    hsla: Object.freeze({ hue: 12, saturation: 85, luminance: 43, alpha: 100 }),
+    hsv: Object.freeze({ hue: 12, saturation: 92, vibrance: 80 }),
+    rgba: Object.freeze({ red: 204, green: 54, blue: 16, alpha: 255 }),
+    rgba32: 0xcc_36_10_ff,
+  }),
+  orange: Object.freeze({
+    argb32: 0xff_db_91_1a & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 0, magenta: 34, yellow: 88, key: 14 }),
+    hex: '#db911aff',
+    hsla: Object.freeze({ hue: 37, saturation: 79, luminance: 48, alpha: 100 }),
+    hsv: Object.freeze({ hue: 37, saturation: 88, vibrance: 86 }),
+    rgba: Object.freeze({ red: 219, green: 145, blue: 26, alpha: 255 }),
+    rgba32: 0xdb_91_1a_ff,
+  }),
+  yellow: Object.freeze({
+    argb32: 0xff_d9_bf_18 & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 0, magenta: 12, yellow: 89, key: 15 }),
+    hex: '#d9bf18ff',
+    hsla: Object.freeze({ hue: 52, saturation: 80, luminance: 47, alpha: 100 }),
+    hsv: Object.freeze({ hue: 52, saturation: 89, vibrance: 85 }),
+    rgba: Object.freeze({ red: 217, green: 191, blue: 24, alpha: 255 }),
+    rgba32: 0xd9_bf_18_ff,
+  }),
+  green: Object.freeze({
+    argb32: 0xff_5f_d6_3e & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 56, magenta: 0, yellow: 71, key: 16 }),
+    hex: '#5fd63eff',
+    hsla: Object.freeze({
+      hue: 107,
+      saturation: 65,
+      luminance: 54,
+      alpha: 100,
+    }),
+    hsv: Object.freeze({ hue: 107, saturation: 71, vibrance: 84 }),
+    rgba: Object.freeze({ red: 95, green: 214, blue: 61, alpha: 255 }),
+    rgba32: 0x5f_d6_3e_ff,
+  }),
+  blue: Object.freeze({
+    argb32: 0xff_22_75_ba & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 82, magenta: 37, yellow: 0, key: 27 }),
+    hex: '#2275baff',
+    hsla: Object.freeze({
+      hue: 207,
+      saturation: 69,
+      luminance: 43,
+      alpha: 100,
+    }),
+    hsv: Object.freeze({ hue: 207, saturation: 82, vibrance: 73 }),
+    rgba: Object.freeze({ red: 34, green: 117, blue: 186, alpha: 255 }),
+    rgba32: 0x22_75_ba_ff,
+  }),
+  purple: Object.freeze({
+    argb32: 0xff_75_32_b3 & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 35, magenta: 72, yellow: 0, key: 30 }),
+    hex: '#7532b3ff',
+    hsla: Object.freeze({
+      hue: 271,
+      saturation: 56,
+      luminance: 45,
+      alpha: 100,
+    }),
+    hsv: Object.freeze({ hue: 271, saturation: 72, vibrance: 70 }),
+    rgba: Object.freeze({ red: 117, green: 50, blue: 179, alpha: 255 }),
+    rgba32: 0x75_32_b3_ff,
+  }),
+  magenta: Object.freeze({
+    argb32: 0xff_de_1d_cb & 0xff_ff_ff_ff,
+    cmyk: Object.freeze({ cyan: 0, magenta: 87, yellow: 9, key: 13 }),
+    hex: '#de1dcbff',
+    hsla: Object.freeze({
+      hue: 306,
+      saturation: 77,
+      luminance: 49,
+      alpha: 100,
+    }),
+    hsv: Object.freeze({ hue: 306, saturation: 87, vibrance: 87 }),
+    rgba: Object.freeze({ red: 222, green: 29, blue: 203, alpha: 255 }),
+    rgba32: 0xde_1d_cb_ff,
   }),
 })
 
 describe('argb32', () => {
   it('should create a color object out of a valid argb value', () => {
-    const color = COLOR_VALUES.magenta
+    const color = COLOR_VALUES_HSLA_BASED.magenta
 
     const colorObj = argb32(color.argb32)
 
@@ -107,16 +195,6 @@ describe('argb32', () => {
     expect(colorObj.toHex()).toEqual(color.hex)
     expect(colorObj.toHsla()).toEqual(color.hsla)
     expect(colorObj.toHsv()).toEqual(color.hsv)
-    expect(colorObj.toRgba()).toEqual(color.rgba)
-  })
-
-  it('should create a color object out of a given number', () => {
-    // Color Def. ==== __AA_RR_GG_BB
-    const color = COLOR_VALUES.blue
-
-    const colorObj = argb32(color.argb32)
-
-    expect(colorObj.toArgb32()).toBe(color.argb32)
     expect(colorObj.toRgba()).toEqual(color.rgba)
   })
 
@@ -129,7 +207,7 @@ describe('argb32', () => {
 
 describe('cmyk', () => {
   it('should create a color object out of a valid cmyk value', () => {
-    const color = COLOR_VALUES.purple
+    const color = COLOR_VALUES_HSLA_BASED.purple
 
     const colorObj = cmyk(color.cmyk)
 
@@ -142,7 +220,7 @@ describe('cmyk', () => {
   })
 
   it('should throw an error when provided out-of-range values', () => {
-    const color = COLOR_VALUES.purple
+    const color = COLOR_VALUES_HSLA_BASED.purple
     const colorCmykCyanOOR = { ...color.cmyk, cyan: 101 }
     const colorCmykMagentaOOR = { ...color.cmyk, magenta: -1 }
     const colorCmykYellowOOR = { ...color.cmyk, yellow: -238 }
@@ -157,7 +235,7 @@ describe('cmyk', () => {
 
 describe('hex', () => {
   it('should create a color object out of a valid hex value', () => {
-    const color = COLOR_VALUES.green
+    const color = COLOR_VALUES_HSLA_BASED.green
 
     const colorObj = hex(color.hex)
 
@@ -170,7 +248,7 @@ describe('hex', () => {
   })
 
   it('should consider rgb hex values', () => {
-    const color = COLOR_VALUES.green
+    const color = COLOR_VALUES_HSLA_BASED.green
     const hexValue = color.hex.slice(0, 7) // #rrggbb
 
     const colorObj = hex(hexValue)
@@ -184,10 +262,8 @@ describe('hex', () => {
   })
 
   it('should consider hex values without "#"', () => {
-    const color = COLOR_VALUES.green
+    const color = COLOR_VALUES_HSLA_BASED.green
     const hexValue = color.hex.replace('#', '')
-
-    console.log('hexValue', hexValue)
 
     const colorObj = hex(hexValue)
 
@@ -206,134 +282,105 @@ describe('hex', () => {
   })
 })
 
-// describe('Color', () => {
-//   it('can be instantiated', () => {
-//     const colorObj = new Color()
+describe('hsla', () => {
+  it('should create a color object out of a valid hsla value', () => {
+    const color = COLOR_VALUES_HSLA_BASED.orange
 
-//     expect(colorObj).toBeTypeOf('object')
-//     expect(colorObj).toHaveProperty('value', -1)
-//   })
+    const colorObj = hsla(color.hsla)
 
-//   it('can be instantiated from Hexadecimal values', () => {
-//     const colorHex = 'fafafa'
-//     const colorObj = Color.fromHex(colorHex)
+    expect(colorObj.toArgb32()).toBe(color.argb32)
+    expect(colorObj.toCmyk()).toEqual(color.cmyk)
+    expect(colorObj.toHex()).toEqual(color.hex)
+    expect(colorObj.toHsla()).toEqual(color.hsla)
+    expect(colorObj.toHsv()).toEqual(color.hsv)
+    expect(colorObj.toRgba()).toEqual(color.rgba)
+  })
 
-//     expect(colorObj.toHex()).toBe(colorHex)
-//   })
+  it('should throw an error when provided out-of-range values', () => {
+    const color = COLOR_VALUES_HSLA_BASED.red
+    const colorHslaHueOOR = { ...color.hsla, hue: 361 }
+    const colorHslaSaturationOOR = { ...color.hsla, saturation: -1 }
+    const colorHslaLuminanceOOR = { ...color.hsla, luminance: -50 }
+    const colorHslaAlphaOOR = { ...color.hsla, alpha: 101 }
 
-//   it('can be instantiated from RGB(A) values', () => {
-//     const colorRgba = {
-//       red: 32,
-//       green: 238,
-//       blue: 124,
-//       alpha: 80,
-//     }
+    expect(() => hsla(colorHslaHueOOR)).toThrowError()
+    expect(() => hsla(colorHslaSaturationOOR)).toThrowError()
+    expect(() => hsla(colorHslaLuminanceOOR)).toThrowError()
+    expect(() => hsla(colorHslaAlphaOOR)).toThrowError()
+  })
+})
 
-//     const colorObj = Color.fromRgba(colorRgba)
+describe('hsv', () => {
+  it('should create a color object out of a valid hsv value', () => {
+    const color = COLOR_VALUES_HSV_BASED.blue
 
-//     expect(colorObj.toRgba()).toEqual(colorRgba)
-//   })
+    const colorObj = hsv(color.hsv)
 
-//   it('can be instantiated from HSL(A) values', () => {
-//     const colorHsla = {
-//       hue: 293,
-//       saturation: 50,
-//       lightness: 89,
-//       alpha: 38,
-//     }
+    expect(colorObj.toArgb32()).toBe(color.argb32)
+    expect(colorObj.toCmyk()).toEqual(color.cmyk)
+    expect(colorObj.toHex()).toEqual(color.hex)
+    expect(colorObj.toHsla()).toEqual(color.hsla)
+    expect(colorObj.toHsv()).toEqual(color.hsv)
+    expect(colorObj.toRgba()).toEqual(color.rgba)
+  })
 
-//     const colorObj = Color.fromHsla(colorHsla)
+  it('should throw an error when provided out-of-range values', () => {
+    const color = COLOR_VALUES_HSV_BASED.magenta
+    const colorHsvHueOOR = { ...color.hsv, hue: 361 }
+    const colorHsvSaturationOOR = { ...color.hsv, saturation: -1 }
+    const colorHsvVibranceOOR = { ...color.hsv, vibrance: -50 }
 
-//     const { hue, saturation, lightness, alpha } = colorObj.toHsla()
+    expect(() => hsv(colorHsvHueOOR)).toThrowError()
+    expect(() => hsv(colorHsvSaturationOOR)).toThrowError()
+    expect(() => hsv(colorHsvVibranceOOR)).toThrowError()
+  })
+})
 
-//     // HSL and RGB requires a lot of calcs, which can result into a difference
-//     // between values, therefore, we check if this difference is between `1`
-//     expect(diff(colorHsla.hue, hue)).toBeLessThanOrEqual(1)
-//     expect(diff(colorHsla.saturation, saturation)).toBeLessThanOrEqual(1)
-//     expect(diff(colorHsla.lightness, lightness)).toBeLessThanOrEqual(1)
-//     expect(diff(colorHsla.alpha, alpha)).toBeLessThanOrEqual(1)
-//   })
+describe('rgba', () => {
+  it('should create a color object out of a valid rgba value', () => {
+    const color = COLOR_VALUES_HSLA_BASED.magenta
 
-//   it('can be used to convert RGB(A) to HSL(A)', () => {
-//     const colorHex = 'FF5733'
-//     const colorRgba = { red: 255, green: 87, blue: 51, alpha: 255 }
-//     const colorHsla = { hue: 11, saturation: 100, lightness: 60, alpha: 100 }
+    const colorObj = rgba(color.rgba)
 
-//     const colorObj = Color.fromHex(colorHex)
-//     const resultRgba = colorObj.toRgba()
-//     const resultHsla = colorObj.toHsla()
+    expect(colorObj.toArgb32()).toBe(color.argb32)
+    expect(colorObj.toCmyk()).toEqual(color.cmyk)
+    expect(colorObj.toHex()).toEqual(color.hex)
+    expect(colorObj.toHsla()).toEqual(color.hsla)
+    expect(colorObj.toHsv()).toEqual(color.hsv)
+    expect(colorObj.toRgba()).toEqual(color.rgba)
+  })
 
-//     expect(resultRgba).toEqual(colorRgba)
-//     expect(resultHsla).toEqual(colorHsla)
-//   })
+  it('should throw an error when provided out-of-range values', () => {
+    const color = COLOR_VALUES_HSLA_BASED.yellow
+    const colorRgbaRedOOR = { ...color.rgba, red: 256 }
+    const colorRgbaBlueOOR = { ...color.rgba, green: 38291 }
+    const colorRgbaGreenOOR = { ...color.rgba, blue: -50 }
+    const colorRgbaAlphaOOR = { ...color.rgba, alpha: -1 }
 
-//   it('can output different color string formats', () => {
-//     const colorHex = '4287f5'
-//     const colorHsl = { hue: 217, saturation: 90, lightness: 61 }
-//     const colorRgb = { red: 66, green: 135, blue: 245 }
-//     const colorA32 = 0xff4287f5
+    expect(() => rgba(colorRgbaRedOOR)).toThrowError()
+    expect(() => rgba(colorRgbaBlueOOR)).toThrowError()
+    expect(() => rgba(colorRgbaGreenOOR)).toThrowError()
+    expect(() => rgba(colorRgbaAlphaOOR)).toThrowError()
+  })
+})
 
-//     const hslString = `hsl(${colorHsl.hue}, ${colorHsl.saturation}%, ${colorHsl.lightness}%)`
-//     const hslaString = `hsla(${colorHsl.hue}, ${colorHsl.saturation}%, ${colorHsl.lightness}%, 1)`
-//     const rgbString = `rgb(${colorRgb.red}, ${colorRgb.green}, ${colorRgb.blue})`
-//     const rgbaString = `rgba(${colorRgb.red}, ${colorRgb.green}, ${colorRgb.blue}, 1)`
-//     const parsedA32 = colorA32 & 0xffffffff
+describe('rgba32', () => {
+  it('should create a color object out of a valid argb value', () => {
+    const color = COLOR_VALUES_HSLA_BASED.purple
 
-//     const colorObj = new Color(colorA32)
+    const colorObj = rgba32(color.rgba32)
 
-//     expect(colorObj.toString()).toEqual(parsedA32.toString())
-//     expect(colorObj.toString('bits')).toEqual(parsedA32.toString())
-//     expect(colorObj.toString('hex')).toEqual(`#${colorHex}`)
-//     expect(colorObj.toString('hexa')).toEqual(`#${colorHex}ff`)
-//     expect(colorObj.toString('hsl')).toEqual(hslString)
-//     expect(colorObj.toString('hsla')).toEqual(hslaString)
-//     expect(colorObj.toString('rgb')).toEqual(rgbString)
-//     expect(colorObj.toString('rgba')).toEqual(rgbaString)
-//   })
+    expect(colorObj.toArgb32()).toBe(color.argb32)
+    expect(colorObj.toCmyk()).toEqual(color.cmyk)
+    expect(colorObj.toHex()).toEqual(color.hex)
+    expect(colorObj.toHsla()).toEqual(color.hsla)
+    expect(colorObj.toHsv()).toEqual(color.hsv)
+    expect(colorObj.toRgba()).toEqual(color.rgba)
+  })
 
-//   it('should not accept unsupported formats when converting', () => {
-//     const colorHex = '9fe2bf'
-
-//     const colorObj = Color.fromHex(colorHex)
-
-//     // @ts-expect-error meant to be a test
-//     expect(() => colorObj.toString('lab')).toThrowError()
-//   })
-
-//   it('should throw an error for invalid Hexadecimal values', () => {
-//     // Based on '9fe2bf'
-//     const colorHexInvalidA = '9fe2gf'
-//     const colorHexInvalidB = '9fe2bffff'
-//     const colorHexInvalidC = '##9fe2bfff'
-
-//     expect(() => Color.fromHex(colorHexInvalidA)).toThrowError()
-//     expect(() => Color.fromHex(colorHexInvalidB)).toThrowError()
-//     expect(() => Color.fromHex(colorHexInvalidC)).toThrowError()
-//   })
-
-//   it('should throw an error for invalid RGB(A) values', () => {
-//     const base = { red: 127, green: 127, blue: 127, alpha: 127 }
-//     const invalidRed = { ...base, red: -327 }
-//     const invalidGreen = { ...base, green: -1 }
-//     const invalidBlue = { ...base, green: 37281 }
-//     const invalidAlpha = { ...base, green: 256 }
-
-//     expect(() => Color.fromRgba(invalidRed)).toThrowError()
-//     expect(() => Color.fromRgba(invalidGreen)).toThrowError()
-//     expect(() => Color.fromRgba(invalidBlue)).toThrowError()
-//     expect(() => Color.fromRgba(invalidAlpha)).toThrowError()
-//   })
-
-//   it('should throw an error for invalid HSL(A) values', () => {
-//     const base = { hue: 136, saturation: 54, lightness: 43, alpha: 100 }
-//     const invalidHue = { ...base, hue: -478 }
-//     const invalidSaturation = { ...base, saturation: -1 }
-//     const invalidLightness = { ...base, lightness: 129 }
-//     const invalidAlpha = { ...base, alpha: 101 }
-
-//     expect(() => Color.fromHsla(invalidHue)).toThrowError()
-//     expect(() => Color.fromHsla(invalidSaturation)).toThrowError()
-//     expect(() => Color.fromHsla(invalidLightness)).toThrowError()
-//     expect(() => Color.fromHsla(invalidAlpha)).toThrowError()
-//   })
-// })
+  it('should throw an error when provided with NaN-like values', () => {
+    const value = 'not a number'
+    // @ts-expect-error for test purposes
+    expect(() => argb32(value)).toThrowError()
+  })
+})
