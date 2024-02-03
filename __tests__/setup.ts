d44d21fd73d257817ca904859bf0254cf9dce4b6
Yuri Uliam <21960752+yuriuliam@users.noreply.github.com>
2024-02-03 07:55:21 -0300

    chore(tests): setup jest-dom matchers

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,4 +1,17 @@
+import { cleanup } from '@testing-library/react'
+import { afterEach } from 'vitest'
+
 import { createStorageMock } from './__mocks__/cache/storage'
+
+import '@testing-library/jest-dom/vitest'
+
+// React Cleanup
+
+afterEach(() => {
+  cleanup()
+})
+
+// Setup Local Storage and Session Storage Mocks
 
 Reflect.set(window, 'localStorage', createStorageMock())
 Reflect.set(window, 'sessionStorage', createStorageMock())
