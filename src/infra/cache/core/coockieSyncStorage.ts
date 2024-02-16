6d5503230e86b1c621ed2a5444d651f014869957
Yuri Uliam <21960752+yuriuliam@users.noreply.github.com>
2024-02-16 08:09:02 -0300

    fix: missing cache storage prefix

--- src/infra/cache/core/coockieSyncStorage.ts
+++ src/infra/cache/core/coockieSyncStorage.ts
@@ -1,14 +1,19 @@
+import { COOKIE_STORAGE_PREFIX } from '@/config/cache'
+
 import { createCookieParser } from '@/data/cache/core/createCookieParser'
 import { createSyncStorage } from '@/data/cache/core/createSyncStorage'
 
 const cookieParser = createCookieParser()
 
-const cookieSyncStorage = createSyncStorage({
-  getItem: key => cookieParser.get(key) ?? null,
-  setItem: (key, value) => void cookieParser.set(key, value),
-  removeItem: key => {
-    cookieParser.remove(key)
+const cookieSyncStorage = createSyncStorage(
+  {
+    getItem: key => cookieParser.get(key) ?? null,
+    setItem: (key, value) => void cookieParser.set(key, value),
+    removeItem: key => {
+      cookieParser.remove(key)
+    },
   },
-})
+  COOKIE_STORAGE_PREFIX,
+)
 
 export { cookieSyncStorage }
