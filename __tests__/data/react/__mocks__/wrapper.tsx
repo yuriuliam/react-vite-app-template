0fb60017950f075df691ea6c2ccf97ee088538ea
Yuri Uliam <21960752+yuriuliam@users.noreply.github.com>
2024-02-24 22:08:18 -0300

    fix: simplified syntax for profilerWrapper

--- __tests__/data/react/__mocks__/wrapper.tsx
+++ __tests__/data/react/__mocks__/wrapper.tsx
@@ -8,22 +8,12 @@ import { AuthProvider } from '@/modules/auth/data/contexts/auth'
 import { FeaturesProvider } from '@/modules/features/data/contexts/features'
 
 type ProfilerProps = React.ComponentProps<typeof Profiler>
-type ProfilerType = React.ComponentType<ProfilerProps>
 
 const AuthWrapper = withProps(AuthProvider, {})
 
 const FeaturesWrapper = withProps(FeaturesProvider, {})
 
-const profilerWrapperWith = withProps.bind<
-  null,
-  [Component: ProfilerType],
-  [propsToBound: ProfilerProps],
-  React.ForwardRefExoticComponent<
-    Omit<ProfilerProps, keyof ProfilerProps> &
-      Partial<ProfilerProps> &
-      React.RefAttributes<unknown>
-  >
->(null, Profiler)
+const profilerWrapperWith = (props: ProfilerProps) => withProps(Profiler, props)
 
 const StoreWrapper = withProps(StoreProvider, {})
 
