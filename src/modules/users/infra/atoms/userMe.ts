0ae16c2bb043d66cc49f1d102561312b6a0b9748
Yuri Uliam <21960752+yuriuliam@users.noreply.github.com>
2024-04-04 08:19:00 -0300

    lint: userMe atom

--- src/modules/users/infra/atoms/userMe.ts
+++ src/modules/users/infra/atoms/userMe.ts
@@ -1,8 +1,9 @@
 import { atomWithLocalStorage } from '@/infra/cache/localSyncStorage'
 
-const [userMe, useUserMe] =
-  atomWithLocalStorage<App.Modules.Users.User | null>('users:me', null, {
-    getOnInit: true,
-  })
+const [userMe, useUserMe] = atomWithLocalStorage<App.Modules.Users.User | null>(
+  'users:me',
+  null,
+  { getOnInit: true },
+)
 
 export { useUserMe, userMe }
