12dd44fbbe455d5d55095a33a4edeef0d12070f8
Yuri Uliam <21960752+yuriuliam@users.noreply.github.com>
2024-04-30 12:55:31 -0300

    fix: route path assertion to throw error + pnpm version

--- src/modules/routing/data/subjects/assertRouteIsParsed.ts
+++ src/modules/routing/data/subjects/assertRouteIsParsed.ts
@@ -1,17 +1,19 @@
+import { assert } from '@/shared/utils/runtime'
+
 type AssertRouteIsParsedFn = App.Modules.Routing.AssertRouteIsParsedFn
 
 const assertRouteIsParsed: AssertRouteIsParsedFn = (route, message) => {
   if (!route) return
 
   if (typeof route === 'string') {
-    console.assert(!route.includes(':'), message)
+    assert(!route.includes(':'), message)
 
     return
   }
 
   if (!route.pathname) return
 
-  console.assert(!route.pathname.includes(':'), message)
+  assert(!route.pathname.includes(':'), message)
 }
 
 export { assertRouteIsParsed }
